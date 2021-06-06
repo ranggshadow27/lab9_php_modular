afcf97d29af114833069f3de81816e62cead4ae7
rangga <46300525+ranggshadow27@users.noreply.github.com>
2021-06-06 07:42:09 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modularisasi PHP</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi Menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="kontak.php">Kontak</a>
+        </nav>
